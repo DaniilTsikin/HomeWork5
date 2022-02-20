2bebba9d67048ca24904adc3981509748d01b2a6
Daniil <dtsikin@mail.ru>
2022-02-20 16:21:41 +0500

    Complete Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу замену элементов массива на противоположные.
+
+int Input()
+{
+    Console.Write("Введите число элементов массива: ");
+    string inp = Console.ReadLine();
+    int.TryParse(inp, out int number);
+    return number;
+}
+
+int[] ArrayInit(int number)
+{
+    int[] array = new int[number];
+    Random randomValue = new Random();
+
+    for(int i = 0; i < number; i++)
+    {
+        array[i] = randomValue.Next(-100, 100);
+    }
+    return array;
+}
+
+void ArrPrint(int[] arr)
+{
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}; ");
+    }
+}
+
+void EditElement(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    }
+}
+
+int value = Input();
+
+int [] arr = ArrayInit(value);
+
+ArrPrint(arr);
+Console.WriteLine();
+EditElement(arr);
+ArrPrint(arr);
